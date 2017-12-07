b070cd8302c566e15fa2ec4c52d1f4735fc6cbc2
Jane <iamjane.gmail.com>
2017-12-06 16:43:30 -0800

    set up articles reducer and put latest articles in props

---
+++ news/src/actions/index.js
@@ -0,0 +1,12 @@
+const URL = `http://localhost:3000`
+
+export function latestNews(){
+  const request = fetch(`${URL}/articles?_order=desc&_end=3`, {method:'GET'})
+  .then(response => response.json())
+
+  return{
+    type:'GET_LATEST',
+    payload: request
+  }
+
+}
